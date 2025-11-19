8d4816a56184c32bc91607f8d8814126d66c1b32
Bastien <bmoulia@gmail.com>
2025-11-19 18:58:09 +0100

    chore(deps): update Angular packages to release candidate 3 and 6
    feat(main): enable zoneless change detection in bootstrap
    
    fix(tsconfig): add esModuleInterop and set moduleResolution to bundler

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -1,6 +1,9 @@
+import { provideZonelessChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [provideZonelessChangeDetection(), ...appConfig.providers],
+}).catch((err) => console.error(err));
